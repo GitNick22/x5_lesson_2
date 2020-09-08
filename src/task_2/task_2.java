56c9dded466f5bb3565c9d2b64765a8e8e014c94
Николай <nngitalov@yandex.ru>
2020-09-09 01:14:01 +0300

    update code task_2

--- src/task_2/task_2.java
+++ src/task_2/task_2.java
@@ -1,4 +1,25 @@
 package task_2;
 
+/*
+Ввести целое трехзначное число в консоли и найти
+сумму цифр этого трехзначного числа
+ */
+
+import java.util.Scanner;
+
 public class task_2 {
+    public static void main (String [] args) {
+        System.out.println("Введите любое трехзначное число: ");
+        Scanner scanner = new Scanner(System.in);
+        int inputValue = scanner.nextInt();
+        int firstNum = inputValue / 100;
+        int secondNum = (inputValue % 100) / 10;
+        int thirdNum = inputValue % 10;
+        int sumNum = firstNum + secondNum + thirdNum;
+        System.out.println("Первая цифра введенного числа: " + firstNum);
+        System.out.println("Вторая цифра введенного числа: " + secondNum);
+        System.out.println("Треться цифра введенного числа: " + thirdNum);
+        System.out.println("Сумма всех чисел: " + sumNum);
+    }
+
 }
